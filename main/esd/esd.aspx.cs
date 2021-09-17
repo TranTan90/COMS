27e54e6733409725a225b4b4ae6ee4098c134adc
TranTan90 <trantan0490@gmail.com>
2021-09-17 10:54:21 +0700

    coms

---
+++ main/esd/esd.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using HanaMicron.COMS.Model;
+using HanaMicron.COMS.BLL;
+using HanaMicron.COMS.Utility;
+
+
+public partial class esd : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            if (!String.IsNullOrEmpty(Request.QueryString["page"])) GridView1.PageIndex = Convert.ToInt32(Request.QueryString["page"]);
+            if (!String.IsNullOrEmpty(Request.QueryString["keyWord"])) ddlKeyWord1.SelectedValue = Request.QueryString["keyWord"];
+            if (!String.IsNullOrEmpty(Request.QueryString["key"])) txtKey1.Text = Request.QueryString["key"];
+
+            GridView1.Visible = false;
+        }
+        else
+        {
+            GridView1.Visible = true;
+        }
+    }
+
+    protected void GridView1_RowDataBound1(object sender, GridViewRowEventArgs e)
+    {
+
+        if (e.Row.RowType == DataControlRowType.DataRow)
+        {
+            VisitDataInfo obj = (VisitDataInfo)e.Row.DataItem;
+
+            e.Row.Cells[0].Text = obj.VisitorDataInfoList[0].VisitDate;
+            e.Row.Cells[1].Text = obj.VisitorDataInfoList[0].VisitorInfo.VisitorName;
+            if (String.IsNullOrEmpty(obj.VisitorDataInfoList[0].VisitorInfo.VisitorPassportNumber))
+            {
+
+                e.Row.Cells[2].Text = obj.VisitorDataInfoList[0].VisitorInfo.VisitorPassportNumber;
+            }
+            else
+            {
+                e.Row.Cells[2].Text = obj.VisitorDataInfoList[0].VisitorInfo.VisitorRegNumber1;
+            }
+            e.Row.Cells[3].Text = obj.VisitorDataInfoList[0].CompanyInfo.CompanyName;
+            e.Row.Cells[4].Text = obj.CarDataInfo.Header + " " + obj.CarDataInfo.Number;
+            e.Row.Cells[5].Text = obj.VisitObjectInfo.VisitObjectName;
+            e.Row.Cells[6].Text = obj.VisitorDataInfoList[0].VisitorInfo.VisitorPhone1 + "" + obj.VisitorDataInfoList[0].VisitorInfo.VisitorPhone2 + "" + obj.VisitorDataInfoList[0].VisitorInfo.VisitorPhone3;
+            var phone = e.Row.Cells[6].ToString();
+
+            VisitData bll = new VisitData();
+
+            if (obj.VisitorDataInfoList[0].VisitorInfo.EsdFlag == "N")
+            {
+                e.Row.Cells[7].Text = "<span style=\"color:red\">No</span>";
+            }
+            else
+            {
+                e.Row.Visible = false;
+            }
+            for (int i = 0; i < e.Row.Cells.Count; i++)
+            {
+                if (i < 6)
+                {
+                    
+                    e.Row.Cells[i].Attributes.Add("onclick", "window.location='/COMS/main/esd/esdview.aspx?visitDataCode=" + obj.VisitDataCode + "&visitorCode=" + obj.VisitorDataInfoList[0].VisitorInfo.VisitorCode + "&mode=esdTime&keyWord=" + ddlKeyWord1.SelectedValue + "&key=" + HttpUtility.UrlEncode(txtKey1.Text) + "&page=" + GridView1.PageIndex + "';");
+                    e.Row.Cells[i].Attributes.Add("style", "cursor:hand;");
+                }
+            }
+
+
+        }
+
+
+    }
+}
---
+++ main/esd/esdview.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="esdview.aspx.cs" Inherits="esdview" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"crossorigin="anonymous">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.js" integrity="sha512-n/4gHW3atM3QqRcbCn6ewmpxcLAHGaDjpEBu4xZd47N0W2oQ+6q7oc3PXstrJYXcbNU1OHdQ1T7pAP+gi5Yu8g=="crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script type="text/javascript">
+    $(function () {
+        $('#name1').css("display", "none");
+        var removeratt = function () {
+            $('#name1').prop("onclick", null);
+        }
+        var addcls = function () {
+            $('#name1').css("display", "block");
+        };
+        setTimeout(addcls, 50000);
+    });
+  
+</script>
+<style type="text/css">
+    
+    .note
+    {
+        width:90px;
+        height:90px;    
+        margin-left:100px;
+        
+    }
+      .note1
+      {
+          display:flex;
+          justify-content:space-between;
+          
+      }  
+      .admin1
+          {
+              height:100px;
+              width:100px;
+               background-color:Blue;
+           }
+</style>
+
+<body>
+    <form id="form1" runat="server" class="note1">
+   
+    <div class="admin1" >1</div>
+    <div class="admin1" >2</div>
+    <div class="admin1" >3</div>
+    </form>
+    <div style="height:200px"></div>
+</body>
+</html>
